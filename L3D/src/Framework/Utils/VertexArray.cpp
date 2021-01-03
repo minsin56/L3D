bfb97d314d54ea34b8f04dcabf3d7b68bcd3524e
minsin56 <42984786+minsin56@users.noreply.github.com>
2021-01-03 09:43:30 -0600

    added vertex array class and ability to draw it the slow way

---
+++ L3D/src/Framework/Utils/VertexArray.cpp
@@ -0,0 +1,13 @@
+﻿#include "VertexArray.h"
+
+#include "LuaBridge/LuaBridge.h"
+
+void VertexArray::Add(glm::vec3 Vec)
+{
+    Items.push_back(Vec);
+}
+
+void VertexArray::Register(lua_State* State)
+{
+    luabridge::getGlobalNamespace(State).beginClass<VertexArray>("VArray").addConstructor<void(*)()>().addFunction("Add",&Add).endClass();
+}
